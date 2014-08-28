42427780225a5c6d7b1284af96c571882667d824
Sean Levin <sean.levin@gmail.com>
2014-08-28 10:39:02 -0400

    fix timer not restarting
    defvar was not calling a second time (switch to setq)
    and use more acceptable global variable names

--- mpages-mode.el
+++ mpages-mode.el
@@ -27,29 +27,29 @@
             (formatted-count num 750)
             "   "
             "Time Elapsed: "
-            (tfmt (time-subtract (current-time) start-time)))))
+            (tfmt (time-subtract (current-time) mpages-mode-start-time)))))
 
 (defun update-word-count-maybe ()
-  (if (boundp 'count-timer)
+  (if (boundp 'mpages-mode-count-timer)
       (update-word-count)))
 
 (defun update-word-count ()
   (setq header-line-format (word-count-string)))
 
 (defun end-timer-stuff ()
-  (cancel-timer count-timer)
+  (cancel-timer mpages-mode-count-timer)
   (setq header-line-format nil)
-  (makunbound 'count-timer)
-  (makunbound 'start-time))
+  (makunbound 'mpages-mode-count-timer)
+  (makunbound 'mpages-mode-start-time))
 
 (defun tfmt (time)
   (format-time-string "%M:%S" time))
 
 (defun setup-time ()
-  (defvar-local start-time (current-time)))
+  (setq mpages-mode-start-time (current-time)))
 
 (defun setup-timer ()
-  (defvar-local count-timer (run-at-time nil 5 'update-word-count-maybe))
+  (setq mpages-mode-count-timer (run-at-time nil 5 'update-word-count-maybe))
   (add-hook 'kill-buffer-hook 'end-timer-stuff nil t))
 
 (defun open-today ()
@@ -73,19 +73,13 @@
 
 ;; maybe function names should be mpages prefix
 
-;;(setup-time)
-;;(word-count-string)
-;; [nil 21498 22316 175884 5 update-word-count nil nil 0] timer when not running?
-;; [nil 21498 24521 908091 5 update-word-count nil nil 0] timer when running?
-
-;; figure out why it doesn't restart when I restart the thing
+;; turn it into a mode
 
 ;; make the timer check happen only if the timer has been defined
 ;; so check for local variable and then do the header format
 ;; that way switching away won't make anything happen
 
 ;; get rid of ? stuff
-
 
 ;; should customizable morning pages directory
 ;; should ask the first time its run to set that
